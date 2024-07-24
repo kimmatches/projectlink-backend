2d540f19134497fd6a909d17bf641e49b4b08f8d
kimmatches <108048109+kimmatches@users.noreply.github.com>
2024-07-24 13:31:10 +0900

    환경설정(DB연결, log기능) 세팅

--- projectlink/src/main/java/projectlink/ProjectlinkApplication.java
+++ projectlink/src/main/java/projectlink/ProjectlinkApplication.java
@@ -2,13 +2,21 @@ package projectlink;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.filter.HiddenHttpMethodFilter;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+
+@SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
 public class ProjectlinkApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProjectlinkApplication.class, args);
 		//test
+	}
+	@Bean
+	HiddenHttpMethodFilter hiddenHttpMethodFilter() {
+		return new HiddenHttpMethodFilter();
 	}
 
 }
