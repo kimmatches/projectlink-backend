3b342fb9f46df4cd88d2fdfe7d7d39b2be0bc694
kimmatches <108048109+kimmatches@users.noreply.github.com>
2024-07-24 11:03:54 +0900

    초기세팅

---
+++ projectlink/src/main/java/projectlink/ProjectlinkApplication.java
@@ -0,0 +1,13 @@
+package projectlink;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectlinkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectlinkApplication.class, args);
+	}
+
+}
---
+++ projectlink/src/test/java/projectlink/ProjectlinkApplicationTests.java
@@ -0,0 +1,13 @@
+package projectlink;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectlinkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
