60ef7d8f47771f7e68d28589e0ed98544eaf17df
Seeyou2000 <valentine0517@naver.com>
2024-07-25 11:14:51 +0900

    AppUserService 구현

---
+++ projectlink/src/main/java/projectlink/services/AppUserService.java
@@ -0,0 +1,87 @@
+package projectlink.services;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+import projectlink.models.AppUser;
+import projectlink.models.Board;
+import projectlink.repositories.AppUserRepository;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+@Service
+@Slf4j
+public class AppUserService implements UserDetailsService {
+
+    private final AppUserRepository appUserRepository;
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    @Autowired
+    public AppUserService(AppUserRepository appUserRepository, BCryptPasswordEncoder bCryptPasswordEncoder) {
+        this.appUserRepository = appUserRepository;
+        this.bCryptPasswordEncoder = bCryptPasswordEncoder;
+    }
+
+    public List<AppUser> getAllUsers() {
+        return appUserRepository.findAll();
+    }
+
+    public AppUser getUserByUsername(String username) {
+        return appUserRepository.findByUsername(username);
+    }
+
+    public AppUser createUser(AppUser appUser) {
+        String encodedPassword = bCryptPasswordEncoder.encode(appUser.getPassword());
+        appUser.setPassword(encodedPassword);
+        return appUserRepository.save(appUser);
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        System.out.println("PRINTING THE USERNAME: " + username);
+        AppUser appUser = appUserRepository.findByUsername(username);
+        if (appUser == null) {
+            log.error("User not found");
+            throw new UsernameNotFoundException("User not found");
+        } else {
+            log.info("User found: {}", username);
+        }
+        Collection<SimpleGrantedAuthority> authorities = new ArrayList<>();
+        return new User(appUser.getUsername(), appUser.getPassword(), authorities);
+    }
+
+    public List<Board> getUsersBoards(String username) {
+        AppUser appUser = appUserRepository.findByUsername(username);
+        if (appUser == null) {
+            throw new UsernameNotFoundException("User not found");
+        }
+        return appUser.getBoards();
+    }
+
+    public AppUser addToUsersRecentBoards(String username, Board board) {
+        AppUser appUser = appUserRepository.findByUsername(username);
+        if (appUser == null) {
+            throw new UsernameNotFoundException("User not found");
+        }
+        List<Board> recentBoards = appUser.getRecentBoards();
+        log.info("Before removal: {}", recentBoards.size());
+        recentBoards.remove(board);
+        log.info("After removal: {}", recentBoards.size());
+        AppUser updatedUser = appUserRepository.save(appUser);
+        List<Board> updatedUserRecentBoards = updatedUser.getRecentBoards();
+        updatedUserRecentBoards.add(0, board);
+        if (updatedUserRecentBoards.size() > 4) {
+            updatedUserRecentBoards.remove(recentBoards.size() - 1);
+        }
+        return appUserRepository.save(updatedUser);
+
+    }
+}
